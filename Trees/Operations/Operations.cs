75044bf70c499ea863b067b77cc4d1d104715af7
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-02-28 22:15:01 +0530

    Add project files.

---
+++ Trees/Operations/Operations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees.Operations
+{
+    public class Operations
+    {
+        public static Node CreateTree()
+        {
+            Node root = new Node(1);
+            root.left= new Node(2);
+            root.right= new Node(3);
+            root.left.left=new Node(4);
+            return root;
+        }
+
+        //Space complexity O(n) Time complexity O(n)
+        public static IList<int> PreorderTraversal(Node root)
+        {
+            List<int> data = new List<int>();
+            if (root == null) return data;
+            Stack<Node> st = new Stack<Node>();
+            st.Push(root);
+            while (st.Any())
+            {
+                root = st.Pop();
+                data.Add(root.data);
+                if (root.right != null)
+                {
+                    st.Push(root.right);
+                }
+                if (root.left != null)
+                {
+                    st.Push(root.left);
+                }
+
+            }
+            return data;
+        }
+    }
+}
---
+++ Trees/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Trees;
+using Trees.Operations;
+
+Node root = new Node(1);
+root.right = new Node(2);
+root.right.left = new Node(3);
+
+var data = Operations.PreorderTraversal(root);
